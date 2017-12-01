7d4e72d5f75b726174ec7cfad52178bfc2886efd
Michael Bayles <michael.k.bayles@gmail.com>
2017-11-30 23:23:18 -0600

    Adding solution and tests for Day 1

--- AdventOfCode2017/AdventOfCode2017/Day1.cs
+++ AdventOfCode2017/AdventOfCode2017/Day1.cs
@@ -1,11 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace AdventOfCode2017
 {
     public class Day1
     {
+        public int Part1(string input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length - 1;i++)
+            {
+                int num = int.Parse(input[i].ToString());
+                int next = int.Parse(input[i + 1].ToString());
+
+                if (num == next)
+                {
+                    sum += num;
+                }                               
+            }
+
+            if (input.Last() == input.First())
+            {
+                int num = int.Parse(input[0].ToString());
+                sum += num;
+            }
+            return sum;
+        }
+
+        public int Part2(string input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                int num = int.Parse(input[i].ToString());
+                int next = int.Parse(input[i + input.Length/2].ToString());
+
+                if (num == next)
+                {
+                    sum += num + num;
+                }
+            }          
+            return sum;
+        }
     }
 }
--- AdventOfCode2017/TestProject/Properties/AssemblyInfo.cs
+++ AdventOfCode2017/TestProject/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdventOfCode2017")]
+[assembly: AssemblyTitle("TestProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdventOfCode2017")]
+[assembly: AssemblyProduct("TestProject")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("97049121-ef2a-4149-b8a5-88a89e10b69a")]
+[assembly: Guid("c57ae5be-a233-4ec4-8472-7c1bf46d2144")]
 
 // Version information for an assembly consists of the following four values:
 //
