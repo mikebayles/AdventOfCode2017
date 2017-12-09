ef0c1cffbf72647188d1339eb6e5aa0477876d27
Michael Bayles <michael.k.bayles@gmail.com>
2017-12-08 23:35:48 -0600

    Done with Day9

---
+++ AdventOfCode2017/AdventOfCode2017/Day9.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public class Day9
+    {
+        public int Part1(string input)
+        {
+            int ret = 0;
+            int currentDepth = 1;
+            bool inGarbage = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                switch (c)
+                {
+                    case '{':
+                        if (!inGarbage)
+                            ret += currentDepth++;
+                        break;
+                    case '}':
+                        if (!inGarbage)
+                            currentDepth--;
+                        break;
+                    case '<':
+                        inGarbage = true;
+                        break;
+                    case '>':
+                        inGarbage = false;
+                        break;
+                    case '!':
+                        i++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return ret;
+        }
+
+        public int Part2(string input)
+        {
+            int ret = 0;
+            bool inGarbage = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                switch (c)
+                {
+                    case '<':
+                        if (inGarbage) ret++;
+                        inGarbage = true;
+                        break;
+                    case '>':
+                        inGarbage = false;
+                        break;
+                    case '!':
+                        i++;
+                        break;
+                    default:
+                        if (inGarbage)
+                            ret++;
+                        break;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ AdventOfCode2017/TestProject/Day9Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Day9Test
+    {
+        [TestMethod]
+        public void TestPart1()
+        {
+            Assert.AreEqual(1, new Day9().Part1("{}"));
+            Assert.AreEqual(6, new Day9().Part1("{{{}}}"));
+            Assert.AreEqual(5, new Day9().Part1("{{},{}}"));
+            Assert.AreEqual(16, new Day9().Part1("{{{},{},{{}}}}"));
+            Assert.AreEqual(1, new Day9().Part1("{<a>,<a>,<a>,<a>}"));
+            Assert.AreEqual(9, new Day9().Part1("{{<ab>},{<ab>},{<ab>},{<ab>}}"));
+            Assert.AreEqual(9, new Day9().Part1("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
+            Assert.AreEqual(3, new Day9().Part1("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
+
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            Assert.AreEqual(0, new Day9().Part2("<>"));
+            Assert.AreEqual(17, new Day9().Part2("<random characters>"));
+            Assert.AreEqual(3, new Day9().Part2("<<<<>"));
+            Assert.AreEqual(2, new Day9().Part2("<{!>}>"));
+            Assert.AreEqual(0, new Day9().Part2("<!!>"));
+            Assert.AreEqual(0, new Day9().Part2("<!!!>>"));
+            Assert.AreEqual(10, new Day9().Part2("<{o\"i!a,<{i<a>"));
+        }
+    }
+}
