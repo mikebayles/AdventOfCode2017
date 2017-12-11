b4ce4225a1d1eb95d195d47cfc77e588edd83cc7
Michael Bayles <michael.k.bayles@gmail.com>
2017-12-11 00:18:59 -0600

    Upload Day 11

---
+++ AdventOfCode2017/AdventOfCode2017/Day11.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public class Day11
+    {
+
+        public int Part1(string input)
+        {
+            return Calculate(input, false);
+        }
+
+        public int Part2(string input)
+        {
+            return Calculate(input, true);
+        }
+
+        private int Calculate(string input, bool part2)
+        {
+            int x = 0;
+            int y = 0;
+
+            List<int> distances = new List<int>();
+
+            foreach (var dir in input.Split(','))
+            {
+                switch (dir)
+                {
+                    case "ne":
+                        y--;
+                        x++;
+                        break;
+                    case "se":
+                        x++;
+                        y++;
+                        break;
+                    case "s":
+                        y += 2;
+                        break;
+                    case "sw":
+                        y++;
+                        x--;
+                        break;
+                    case "nw":
+                        x--;
+                        y--;
+                        break;
+                    case "n":
+                        y -= 2;
+                        break;
+                    default:
+                        throw new Exception("Weird direction " + dir);
+
+                }
+
+                distances.Add((int)((Math.Abs(x) + Math.Abs(y)) / 2.0));
+            }
+
+            return part2 ? distances.Max() : distances.Last();
+        }
+    }
+}
---
+++ AdventOfCode2017/TestProject/Day11Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AdventOfCode2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Day11Test
+    {
+        [TestMethod]
+        public void TestPart1()
+        {
+            Assert.AreEqual(3, new Day11().Part1("ne,ne,ne"));
+            Assert.AreEqual(0, new Day11().Part1("ne,ne,sw,sw"));
+            Assert.AreEqual(2, new Day11().Part1("ne,ne,s,s"));
+            Assert.AreEqual(3, new Day11().Part1("se,sw,se,sw,sw"));
+            Assert.AreEqual(682, new Day11().Part1(File.ReadAllText("Day11.txt")));
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            Assert.AreEqual(1406, new Day11().Part2(File.ReadAllText("Day11.txt")));
+        }
+    }
+}
