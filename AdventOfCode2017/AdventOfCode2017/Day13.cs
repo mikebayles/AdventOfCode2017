5c011f950129306d7ace211038820761e3c5c811
Michael Bayles <michael.k.bayles@gmail.com>
2017-12-13 00:38:20 -0600

    Day 13 done

---
+++ AdventOfCode2017/AdventOfCode2017/Day13.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public class Day13
+    {
+        private readonly List<FireWall> firewalls = new List<FireWall>();
+
+        public Day13(string[] input)
+        {
+            foreach (var line in input)
+            {
+                var parts = line.Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                int depth = int.Parse(parts[0]);
+                int range = int.Parse(parts[1]);
+
+                firewalls.Add(new FireWall(depth, range));
+            }
+        }
+        public int Part1()
+        {
+            return CalculateSeverity(0);
+        }
+
+        public int Part2()
+        {
+            int delay = 0;
+            while (CalculateSeverity(delay) > 0)
+                delay++;
+
+            return delay;
+        }
+
+        private int CalculateSeverity(int delay)
+        {
+            int severity = 0;
+
+            foreach (var firewall in firewalls)
+            {
+                if ((firewall.Depth + delay) % (2 * firewall.Range - 2) == 0)
+                {
+                    severity += (firewall.Depth * firewall.Range);
+                    if (delay != 0) return 1;
+                }
+            }
+
+            return severity;
+        }
+    }
+
+    public class FireWall
+    {
+        public int Depth { get; }
+        public int Range { get; }
+
+        public FireWall(int depth, int range)
+        {
+            Depth = depth;
+            Range = range;
+        }
+    }
+}
--- AdventOfCode2017/AdventOfCode2017/Program.cs
+++ AdventOfCode2017/AdventOfCode2017/Program.cs
@@ -8,6 +8,8 @@ namespace AdventOfCode2017
         public static void Main(string[] args)
         {
             var input = File.ReadAllLines("input.txt");
+            Console.WriteLine(new Day13(input).Part1());
+            Console.WriteLine(new Day13(input).Part2());
         }
     }
 }
