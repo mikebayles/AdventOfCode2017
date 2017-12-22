805bc7bcfdea6d04d58bfe6a04b9077852fe455f
Michael Bayles <michael.k.bayles@gmail.com>
2017-12-22 00:14:27 -0600

    Done with Day22. still stuck on Day21

---
+++ AdventOfCode2017/AdventOfCode2017/Day21.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2017
+{
+    public class Day21
+    {
+        private List<Rule> rules;
+
+        public Day21(string[] input)
+        {
+            rules = input.Select(a => new Rule(a.Split()[0], a.Split()[2])).ToList();
+        }
+
+        public int Part1(int iterations)
+        {
+            char[,] drawing = stringToCharArr(".#./..#/###");
+            while (iterations-- > 0)
+            {
+                int size = drawing.GetLength(0);
+
+                var newDrawings = ArraySplit(drawing, size % 2 == 0 ? 2 : 3);
+                var drawingsToMerge = new List<char[,]>();
+
+                foreach (var newDrawing in newDrawings)
+                {
+                    var asString = charArrToString(newDrawing);
+                    var rule = rules.First(a => a.Matches.Any(m => m == asString));
+                    if (rule != null)
+                    {
+                        asString = rule.Apply;
+                    }
+
+                    drawingsToMerge.Add(stringToCharArr(asString));
+
+                }
+
+                drawing = merge(drawingsToMerge);
+            }
+
+
+            return charArrToString(drawing).Count(a => a == '#');
+        }
+
+        private char[,] merge(List<char[,]> arrays)
+        {
+            int size = arrays[0].GetLength(0);
+            int newSize = (int)Math.Sqrt(size * size * arrays.Count);
+
+            var ret = new char[newSize, newSize];
+
+            for (int i = 0; i < arrays.Count; i++)
+            {
+                for (int r = 0; r < size; r++)
+                {
+                    for (int c = 0; c < size; c++)
+                    {
+                        int ri = (r / size) >= 1 ? (r / size) + 1 : (r / size);
+                        int ci = c + size * (i % size);
+                        ret[r + size * ((i + 1) / size), c + size * (i % (int)Math.Sqrt(arrays.Count))] = arrays[i][r, c];
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        public static char[,] stringToCharArr(string drawing)
+        {
+            var rows = drawing.Split('/');
+            var ret = new char[rows.Length, rows.Length];
+            for (int r = 0; r < rows.Length; r++)
+            {
+                var row = rows[r];
+                for (int c = 0; c < row.Length; c++)
+                {
+                    ret[r, c] = row[c];
+                }
+            }
+
+            return ret;
+        }
+
+        public static string charArrToString(char[,] array)
+        {
+            StringBuilder ret = new StringBuilder();
+            int rows = array.GetLength(0);
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < rows; c++)
+                {
+                    ret.Append(array[r, c]);
+                }
+                ret.Append("/");
+            }
+
+            return ret.ToString().TrimEnd('/');
+        }
+
+        private List<char[,]> ArraySplit(char[,] array, int size)
+        {
+            List<char[,]> ret = new List<char[,]>();
+            for (int i = 0; i < (array.GetLength(0) * array.GetLength(1)) / (size * size); i++)
+            {
+                ret.Add(new char[size, size]);
+            }
+
+
+            for (int r = 0; r < array.GetLength(0); r++)
+            {
+                for (int c = 0; c < array.GetLength(1); c++)
+                {
+                    int n = (r * array.GetLength(0)) + c;
+                    if (ret.Count == 1)
+                        ret[0][r % size, c % size] = array[r, c];
+                    else
+                        ret[(n % array.GetLength(0) / size)][r % size, c % size] = array[r, c];
+                }
+            }
+
+            foreach (var a in ret)
+            {
+                if (charArrToString(a).Contains(default(char)))
+                {
+                    Console.WriteLine("adf");
+                }
+            }
+
+
+
+            return ret;
+        }
+    }
+
+    public class Rule
+    {
+        public List<string> Matches { get; }
+        public string Apply { get; }
+
+        private string match;
+
+        public Rule(string match, string apply)
+        {
+            this.match = match;
+            Matches = new List<string>();
+            Apply = apply;
+
+            Matches.Add(match);
+            Matches.Add(Day21.charArrToString(flip(Day21.stringToCharArr(Matches[0]), true)));
+            Matches.Add(Day21.charArrToString(flip(Day21.stringToCharArr(Matches[0]), false)));
+            createRotations();
+            foreach (var s in Matches)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private void createRotations()
+        {
+            foreach (var i in Enumerable.Range(0, Matches.Count))
+            {
+                Matches.Add(Day21.charArrToString(rotate(Day21.stringToCharArr(Matches[i]))));
+            }
+        }
+
+        private char[,] flip(char[,] arr, bool verticle)
+        {
+            var size = arr.GetLength(0);
+            var ret = new char[size, size];
+
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    if (verticle)
+                        ret[r, c] = arr[(size - r - 1), c];
+                    else
+                        ret[r, c] = arr[r, (size - c - 1)];
+                }
+            }
+
+            return ret;
+        }
+
+        private char[,] rotate(char[,] arr)
+        {
+            var size = arr.GetLength(0);
+
+            var ret = new char[size, size];
+
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    ret[r, c] = arr[size - c - 1, r];
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ AdventOfCode2017/AdventOfCode2017/Day22.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace AdventOfCode2017
+{
+    public class Day22
+    {
+        private byte[,] arr;
+        private int rows;
+        private int cols;
+        private int size = 40000;
+
+        private const byte clean = 0;
+        private const byte infected = 1;
+        private const byte weakened = 2;
+        private const byte flagged = 3;
+
+        public Day22(string[] input)
+        {
+
+            arr = new byte[size, size];
+
+            rows = input.Length;
+            cols = input[0].Length;
+
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    arr[r + (size / 2), c + (size / 2)] = (input[r][c] == '#' ? infected : clean);
+                }
+            }
+        }
+
+        public int Part1()
+        {
+            int currentR = (rows / 2) + (size / 2);
+            int currentC = (cols / 2) + (size / 2);
+            int bursts = 0;
+            int infectedCount = 0;
+            char direction = 'X';
+
+            while (bursts < 10000)
+            {
+                byte currentNode = arr[currentR, currentC];
+
+                if (currentNode == infected)
+                {
+                    direction = NextDirection(direction, 'R');
+                    arr[currentR, currentC] = clean;
+                }
+                else if (currentNode == clean)
+                {
+                    direction = NextDirection(direction, 'L');
+                    arr[currentR, currentC] = infected;
+                    infectedCount++;
+                }
+
+                if (direction == 'L')
+                    currentC--;
+                else if (direction == 'U')
+                    currentR--;
+                else if (direction == 'R')
+                    currentC++;
+                else if (direction == 'D')
+                    currentR++;
+
+                bursts++;
+            }
+
+            return infectedCount;
+        }
+
+        public int Part2()
+        {
+            int currentR = (rows / 2) + (size / 2);
+            int currentC = (cols / 2) + (size / 2);
+            int bursts = 0;
+            int infectedCount = 0;
+            char direction = 'U';
+
+            while (bursts < 10000000)
+            {
+                byte currentNode = arr[currentR, currentC];
+
+                if (currentNode == clean)
+                {
+                    arr[currentR, currentC] = weakened;
+                    direction = NextDirection(direction, 'L');
+                }
+                else if (currentNode == weakened)
+                {
+                    arr[currentR, currentC] = infected;
+                    infectedCount++;
+                }
+                else if (currentNode == infected)
+                {
+                    arr[currentR, currentC] = flagged;
+                    direction = NextDirection(direction, 'R');
+                }
+                else if (currentNode == flagged)
+                {
+                    arr[currentR, currentC] = clean;
+                    direction = ReverseDirection(direction);
+                }
+
+                if (direction == 'L')
+                    currentC--;
+                else if (direction == 'U')
+                    currentR--;
+                else if (direction == 'R')
+                    currentC++;
+                else if (direction == 'D')
+                    currentR++;
+
+                bursts++;
+            }
+
+            return infectedCount;
+        }
+
+        private char NextDirection(char lastDirection, char turn)
+        {
+            switch (lastDirection)
+            {
+                case 'U':
+                    return turn == 'L' ? 'L' : 'R';
+                case 'R':
+                    return turn == 'L' ? 'U' : 'D';
+                case 'D':
+                    return turn == 'L' ? 'R' : 'L';
+                case 'L':
+                    return turn == 'L' ? 'D' : 'U';
+                case 'X':
+                    return turn;
+                default:
+                    throw new Exception("Unknown direction " + lastDirection);
+            }
+        }
+
+        private char ReverseDirection(char lastDirection)
+        {
+            switch (lastDirection)
+            {
+                case 'U':
+                    return 'D';
+                case 'R':
+                    return 'L';
+                case 'D':
+                    return 'U';
+                case 'L':
+                    return 'R';
+                default:
+                    throw new Exception("Unknown direction " + lastDirection);
+            }
+        }
+    }
+}
